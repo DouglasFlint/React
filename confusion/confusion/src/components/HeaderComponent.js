46d33725363b5ede15e2654d4ccc46eb982aa248
DouglasFlint <72120678+DouglasFlint@users.noreply.github.com>
2021-04-21 13:51:04 -0500

    Uncontrolled Forms

--- confusion/confusion/src/components/HeaderComponent.js
+++ confusion/confusion/src/components/HeaderComponent.js
@@ -1,77 +1,5 @@
  import React, {Component} from 'react';
-// import {Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron} from 'reactstrap';
-// import{NavLink} from 'react-router-dom';
-
-// class Header extends Component{
-
-//     constructor(props){
-//         super(props);
-        
-//         this.toggleNav = this.toggleNav.bind(this);
-//         this.state = {
-//             isNavOpen: false
-//         };
-//     }
-
-//     toggleNav() {
-//         this.setSate({
-//             isNavOpen: !this.state.isNavOpen
-//         });
-//     }
-
-//     render(){
-//         return(
-//             <div>
-//                 <Navbar dark expand="md">
-//                     <div className="container">
-//                         <NavbarToggler onClick={this.toggleNav}/>
-//                         <NavbarBrand className="mr-auto" href="/">
-//                             <img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion'></img>
-//                         </NavbarBrand>
-//                         <Collapse isOpen={this.state.isNavOpen} navbar>
-//                         <Nav navbar>
-//                             <NavItem>
-//                                 <NavLink className="nav-link" to='/home'>
-//                                     <span className="fa fa-home fa-lg"></span> Home
-//                                 </NavLink>
-//                             </NavItem>
-//                             <NavItem>
-//                                 <NavLink className="nav-link" to='/aboutus'>
-//                                     <span className="fa fa-info fa-lg"></span> About Us
-//                                 </NavLink>
-//                             </NavItem>
-//                             <NavItem>
-//                                 <NavLink className="nav-link" to='/menu'>
-//                                     <span className="fa fa-list fa-lg"></span> Menu
-//                                 </NavLink>
-//                             </NavItem>
-//                             <NavItem>
-//                                 <NavLink className="nav-link" to='/contactus'>
-//                                     <span className="fa fa-address-card fa-lg"></span> Contact Us
-//                                 </NavLink>
-//                             </NavItem>
-//                         </Nav>
-//                         </Collapse>
-//                     </div>
-//                 </Navbar>
-//                 <Jumbotron>
-//                     <div className="container">
-//                         <div className="row row-header">
-//                             <div className="col-12 col-smn-6">
-//                                 <h1>Ristorante Con Fusion</h1>
-//                                 <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </Jumbotron>
-//             </div>
-//         );
-//     }
-// }
-
-// export default Header;
-
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody, FormGroup, Label, Input, Form } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -79,8 +7,11 @@ class Header extends Component {
         super(props);
     
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
         this.state = {
-          isNavOpen: false
+          isNavOpen: false,
+          isModalOpen: false
         };
       }
 
@@ -88,6 +19,19 @@ class Header extends Component {
         this.setState({
           isNavOpen: !this.state.isNavOpen
         });
+      }
+
+      toggleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+          });
+      }
+
+      handleLogin(event){
+        this.toggleModal();
+        alert("Username: " + this.username.value + "Password: " + this.password.value
+        + " Remember: " + this.remember.checked);
+        event.preventDefault();
       }
 
     render() {
@@ -112,6 +56,13 @@ class Header extends Component {
                                 <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                             </NavItem>
                             </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}>
+                                        <span className="fa fa-sign-in fa-lg"></span> Login
+                                    </Button>
+                                </NavItem>
+                            </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
@@ -125,6 +76,31 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input}></Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}></Input>
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                        innerRef={(input) => this.remember = input}/>
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </div>
         );
     }
