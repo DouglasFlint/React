3868fd596949950a9c642b4d238191163e49e929
DouglasFlint <72120678+DouglasFlint@users.noreply.github.com>
2021-04-21 11:39:16 -0500

    Controlled Forms

--- confusion/confusion/src/components/MainComponent.js
+++ confusion/confusion/src/components/MainComponent.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
+
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+
 import {DISHES} from '../shared/dishes';
 import {COMMENTS} from '../shared/comments';
 import {LEADERS} from '../shared/leaders';
 import {PROMOTIONS} from '../shared/promotions';
+
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
@@ -35,6 +39,14 @@ class Main extends Component {
         );
       }
 
+      const AboutUsPage = () => {
+        return(
+          <About
+              leaders = {this.state.leaders}
+          />
+        )
+      }
+
       const DishWithId = ({match}) => {
         return(
           <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
@@ -51,6 +63,7 @@ class Main extends Component {
               <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
               <Route path="/menu/:dishId" component={DishWithId}/>
               <Route exact path="/contactus" component={Contact}/>
+              <Route exact path="/aboutus" component={AboutUsPage}/>
               <Redirect to="/home"/>
             </Switch>
           <Footer/>
