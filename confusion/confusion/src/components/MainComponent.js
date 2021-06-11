6cb6d2e11330877a77485a439bd605137b244e01
DouglasFlint <72120678+DouglasFlint@users.noreply.github.com>
2021-06-11 17:27:50 -0500

    React-Redux Form rivisited

--- confusion/confusion/src/components/MainComponent.js
+++ confusion/confusion/src/components/MainComponent.js
@@ -8,6 +8,7 @@ import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {addComment, fetchDishes} from '../redux/ActionCreators';
+import {actions} from 'react-redux-form';
 
 
 
@@ -25,7 +26,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
 });
 
 class Main extends Component {
@@ -79,7 +81,7 @@ class Main extends Component {
               <Route path="/home" component={HomePage} />
               <Route exact path="/menu" component={()=><Menu dishes={this.props.dishes}/>}/>
               <Route path="/menu/:dishId" component={DishWithId}/>
-              <Route exact path="/contactus" component={Contact}/>
+              <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}></Contact>}/>
               <Route exact path="/aboutus" component={AboutUsPage}/>
               <Redirect to="/home"/>
             </Switch>
