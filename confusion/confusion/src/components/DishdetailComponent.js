a0a33803d89dce6b4dfa480dcc7754a6c09cd195
DouglasFlint <72120678+DouglasFlint@users.noreply.github.com>
2021-06-14 00:10:46 -0500

    Final commit

--- confusion/confusion/src/components/DishdetailComponent.js
+++ confusion/confusion/src/components/DishdetailComponent.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem, CardBody, CardText } from "reactstrap";
+import { Link } from 'react-router-dom';
 import {
     Button, Modal, ModalBody, ModalHeader, Label, Row, Col
 } from "reactstrap";
+
 import { Control, LocalForm, Errors } from 'react-redux-form';
-import {Loading} from './LoadingComponent';
-import {baseUrl} from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
-
+/**........................ comment component ends ................................................. */
+//// validators
 const required = (val) => val && val.length; //value > 0
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
-
 
 class CommentForm extends Component {
 
@@ -31,8 +34,11 @@ class CommentForm extends Component {
     }
 
     handleCommentFormSubmit(values) {
-        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
+        console.log("Current State is: " + JSON.stringify(values));
+        // alert("Current State is: " + JSON.stringify(values));
 
+        // action obj
+        this.props.postComment( this.props.dishId, values.rating, values.author, values.comment );
 
     }
 
@@ -159,62 +165,103 @@ class CommentForm extends Component {
     }
 }
 
+/**........................ comment component ends ................................................. */
 
 
-    function RenderDish({dish}){
-        if (dish != null){
-            return(
-                <Card>
-                    <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
-                    <CardBody>
-                         <CardTitle>{dish.name}</CardTitle>
-                         <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+
+
+
+
+
+
+    function RenderDish({dish}) {
+        if (dish != null) {
+            return (
+                <div className='col-12 col-md-5 m-1'>
+                    <FadeTransform
+                        in
+                        transformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+
+                    <Card>
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle> {dish.name}</CardTitle>
+                            <CardText> {dish.description} </CardText>
+                        </CardBody>
+                    </Card>
+
+                </FadeTransform>
+
+                </div>   
             );
         }
         else {
-            return(
+            return (
                 <div></div>
             );
         }
     }
 
-    function RenderComments({dish, comments, postComment, dishId}){
-        if(comments == null){
-            return(<div></div>)
+    function RenderComments({ dish, comments, postComment, dishId }){
+        if (comments == null) {
+            return (<div></div>)
         }
-
         const cmnts = comments.map(comment => {
-            return(
+            return (
+                
                 <li key={comment.id}>
                     <p>{comment.comment}</p>
                     <p>-- {comment.author},
                     &nbsp;
-                    {new Intl.DateTimeFormat('en-Us',{
+                    {new Intl.DateTimeFormat('en-US', {
                         year: 'numeric',
-                        month: 'short',
+                        month: 'long',
                         day: '2-digit'
-                    }).format(new Date(Date.parse(comment.date)))}
+                    }).format(new Date(comment.date))}
                     </p>
                 </li>
+
             )
         })
-        return(
-            <div className='col-12 col-md m-1'>
+        return (
+            <div className='col-12 col-md-5 m-1'>
                 <h4> Comments </h4>
                 <ul className='list-unstyled'>
                     {cmnts}
                 </ul>
-                <CommentForm  dishId={dishId} 
-                    postComment={postComment}/>
+
+                <Stagger in>
+                    {comments.map((comment) => {
+                        return (
+                            <Fade in>
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                            </Fade>
+                        );
+                    })}
+                </Stagger>
+
+                <CommentForm 
+                    dish={dish} 
+                    comments={comments} 
+                    dishId={dishId} 
+                    postComment={postComment} 
+                />
             </div>
         )
     }
 
-    const DishDetail= (props)=>{
-        if(props.isLoading){
-            return(
+
+    const DishDetail = (props) => {
+
+        const dish = props.dish
+        
+        if (props.isLoading) {
+            return (
                 <div className="container">
                     <div className="row">
                         <Loading />
@@ -222,8 +269,8 @@ class CommentForm extends Component {
                 </div>
             );
         }
-        else if(props.errMess){
-            return(
+        else if (props.errMess){
+            return (
                 <div className="container">
                     <div className="row">
                         <h4>{props.errMess}</h4>
@@ -231,38 +278,43 @@ class CommentForm extends Component {
                 </div>
             );
         }
-        const dish = props.dish
-
-        if(dish == null){
+        else if (props.dish == null) {
             return (<div></div>);
         }
+        else if (props.dish != null) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <Link to="/menu">Menu</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>
+                                { props.dish.name }
+                            </BreadcrumbItem>
+                        </Breadcrumb>
+    
+                        <div className="col-12">
+                            <h3> {props.dish.menu}</h3>
+                            <hr />
+                        </div>
+                    </div>
+    
+                    <div className='row'>
+                        <RenderDish dish={ props.dish } />
+                        <RenderComments dish={ props.dish } comments={ props.comments } 
+                            postComment={ props.postComment }
+                            dishId={ props.dish.id }
+                        />
+                    </div>
+    
+    
+                </div>
+            )
+            
+        }
 
-        // const dishItem = <RenderDish dish={props.dish}/>
-        // const dishComment = <RenderComments comments={props.comments}/>
-
-        return (
-            <div className="container">
-            <div className="row">
-                <Breadcrumb>
-
-                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
-                    <hr />
-                </div>                
-            </div>
-            <div className="row">
-                    <RenderDish dish={props.dish} />
-                    <RenderComments dish={props.dish} comments={props.comments} 
-                        postComment={props.postComment}
-                        dishId={props.dish.id}/>
-            </div>
-            </div>
-        );
     }
-
 
 
 
--- confusion/confusion/src/redux/ActionCreators.js
+++ confusion/confusion/src/redux/ActionCreators.js
@@ -1,73 +1,154 @@
 import * as ActionTypes from './ActionTypes';
-import {DISHES} from '../shared/dishes';
-import {baseUrl} from '../shared/baseUrl';
+// import { DISHES } from '../shared/dishes';
+import { baseUrl } from '../shared/baseUrl';
 
-
-
+/**.......... Comment............................ */
 export const addComment = (comment) => ({
     type: ActionTypes.ADD_COMMENT,
     payload: comment
 });
 
-export const postComment = (dishId, rating, author, comment) => (dispatch) => {
+
+export const postComment = (dishId, rating, author, comment)=> (dispatch)=> {
 
     const newComment = {
         dishId: dishId,
         rating: rating,
         author: author,
-        comment: comment 
+        comment: comment,
     }
     newComment.date = new Date().toISOString();
 
-    return fetch(baseUrl + 'comments', {
+    return fetch( baseUrl + 'comments', {
         method: 'POST',
         body: JSON.stringify(newComment),
         headers: {
-            'Content-Type': 'application/json'
+            'Content-type': 'application/json'
+        },
+        credentials: 'same-origin'
+    })
+    .then(response => {
+
+        if (response.ok) {
+                return response;
+        }
+        else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText)
+                error.response = response;
+                throw error;
+        }
+    },
+        ////if no responmse from server 
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        }
+    )
+    .then( response => response.json() )
+    .then( response => dispatch(addComment(response)) )
+    .catch(error => {
+        console.log('Post Comments', error.message); 
+        alert('Comment could not be posted\nError'+ error.message);
+    });
+
+}
+
+
+
+
+
+/**.......... Feedback ............................ */
+// export const addFeedback = (feedback) => ({
+//     type: ActionTypes.ADD_FEEDBACK,
+//     payload: feedback
+// });
+
+
+
+export const postFeedback = (firstname, lastname, telnum, email, agree, contactType, message) => (dispatch) => {
+
+    const newFeedback = {
+        firstname: firstname,
+        lastname: lastname,
+        telnum: telnum,
+        email: email,
+        agree: agree,
+        contactType: contactType,
+        message: message,
+    }
+    newFeedback.date = new Date().toISOString();
+
+    return fetch(baseUrl + 'feedback', {
+        method: 'POST',
+        body: JSON.stringify(newFeedback),
+        headers: {
+            'Content-type': 'application/json'
         },
         credentials: 'same-origin'
     })
         .then(response => {
-            if(response.ok){
+
+            if (response.ok) {
+                
                 return response;
             }
             else {
-                var error = new Error('Error ' + response.status + ': ' + response.statusText);
-                error.response =  response;
+                var error = new Error('Error ' + response.status + ': ' + response.statusText)
+                error.response = response;
                 throw error;
             }
-        }, 
-        error => {
-            var errmess = new Error(error.message);
-            throw errmess;
-        })
+        },
+            ////if no responmse from server 
+            error => {
+                var errmess = new Error(error.message);
+                throw errmess;
+            }
+        )
         .then(response => response.json())
-        .then(response => dispatch(addComment(response)))
-        .catch(error => {console.log('Post comments', error.message)
-         alert('Your comment could not be posted/nError> ' + console.error.message)})
+        .then(response => alert(JSON.stringify(response)))
+        .catch(error => {
+            console.log('Post Comments', error.message);
+            alert('Comment could not be posted\nError' + error.message);
+        });
+
 }
 
+
+
+
+
+
+
+
+
+
+
+/**.......... Dishes............................ */
+//// thunk: function returns an func
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
 
     return fetch(baseUrl + 'dishes')
         .then(response => {
-            if(response.ok){
+            if (response.ok) 
+            {
                 return response;
             }
-            else {
-                var error = new Error('Error ' + response.status + ': ' + response.statusText);
-                error.response =  response;
+            else
+            {
+                var error = new Error('Error ' + response.status + ': '+ response.statusText )
+                error.response = response;
                 throw error;
             }
-        }, 
-        error => {
-            var errmess = new Error(error.message);
+        },
+         ////if no responmse from server 
+         error => {
+            var errmess = new Error( error.message );
             throw errmess;
-        })
+         })
         .then(response => response.json())
         .then(dishes => dispatch(addDishes(dishes)))
-        .catch(error => dispatch(dishesFailed(error.message)));
+        .catch(error => dispatch(dishesFailed(error.message) ) );
 }
 
 export const dishesLoading = () => ({
@@ -79,33 +160,46 @@ export const dishesFailed = (errmess) => ({
     payload: errmess
 });
 
+//function returns an action
 export const addDishes = (dishes) => ({
     type: ActionTypes.ADD_DISHES,
     payload: dishes
 });
 
+
+
+
+
+
+
+
+
+
+/**.......... Comments............................ */
+//// thunk: function returns an func
 export const fetchComments = () => (dispatch) => {
     return fetch(baseUrl + 'comments')
         .then(response => {
-            if(response.ok){
+            if (response.ok) {
                 return response;
             }
             else {
-                var error = new Error('Error ' + response.status + ': ' + response.statusText);
-                error.response =  response;
+                var error = new Error('Error ' + response.status + ': ' + response.statusText)
+                error.response = response;
                 throw error;
             }
-        }, 
+        },
+        ////if no responmse from server 
         error => {
             var errmess = new Error(error.message);
             throw errmess;
-        })
+        }) 
         .then(response => response.json())
         .then(comments => dispatch(addComments(comments)))
         .catch(error => dispatch(commentsFailed(error.message)));
-}
+};
 
-export const commnetsFailed = (errmess) => ({
+export const commentsFailed = (errmess) => ({
     type: ActionTypes.COMMENTS_FAILED,
     payload: errmess
 });
@@ -115,20 +209,25 @@ export const addComments = (comments) => ({
     payload: comments
 });
 
+
+
+/**.......... promos............................ */
 export const fetchPromos = () => (dispatch) => {
-    dispatch(promosLoading(true));
+
+    dispatch(promosLoading());
 
     return fetch(baseUrl + 'promotions')
         .then(response => {
-            if(response.ok){
+            if (response.ok) {
                 return response;
             }
             else {
-                var error = new Error('Error ' + response.status + ': ' + response.statusText);
-                error.response =  response;
+                var error = new Error('Error ' + response.status + ': ' + response.statusText)
+                error.response = response;
                 throw error;
             }
-        }, 
+        },
+        ////if no responmse from server 
         error => {
             var errmess = new Error(error.message);
             throw errmess;
@@ -152,3 +251,45 @@ export const addPromos = (promos) => ({
     payload: promos
 });
 
+
+
+
+/**.......... leaders............................ */
+export const fetchLeaders = () => (dispatch) => {
+
+    dispatch( leadersLoading() );
+
+    return fetch( baseUrl + 'leaders')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            }
+            else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText)
+                error.response = response;
+                throw error;
+            }
+        },
+        ////if no responmse from server 
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then( leaders => dispatch( addLeaders(leaders) ) ) 
+        .catch(error => dispatch(leadersFailed(error.message)));
+}
+
+export const leadersLoading = () => ({
+    type: ActionTypes.LEADERS_LOADING
+});
+
+export const leadersFailed = (errmess) => ({
+    type: ActionTypes.LEADERS_FAILED,
+    payload: errmess
+});
+
+export const addLeaders = (leaders) => ({
+    type: ActionTypes.ADD_LEADERS,
+    payload: leaders
+});
